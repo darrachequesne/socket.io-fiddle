a73b457047d31ba73037db03f90ac28118bb0c71
dx9s <dx9s@s76>
2017-07-07 07:27:01 -0700

    changed whitespace to match upstream

--- server-ssl.js
+++ server-ssl.js
@@ -3,8 +3,8 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 const server = require('https').createServer({
-	key: fs.readFileSync('./key.pem'),
-	cert: fs.readFileSync('./cert.pem'),
+  key: fs.readFileSync('./key.pem'),
+  cert: fs.readFileSync('./cert.pem'),
 }, app);
 const io = require('socket.io')(server);
 const port = process.env.PORT || 3001;
@@ -15,7 +15,7 @@ io.on('connect', onConnect);
 server.listen(port, () => console.log('server listening on port ' + port));
 
 function onConnect(socket) {
-	console.log('connect ' + socket.id);
+  console.log('connect ' + socket.id);
 
-	socket.on('disconnect', () => console.log('disconnect ' + socket.id));
+  socket.on('disconnect', () => console.log('disconnect ' + socket.id));
 }
